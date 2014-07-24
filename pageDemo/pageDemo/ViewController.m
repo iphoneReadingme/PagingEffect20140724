a14441f3210a96e46710b27bf594e507943e9c45
iphoneReadingme <readingme@126.com>
2014-07-24 11:55:23 +0800

    翻页动画 添加文本视图

--- pageDemo/pageDemo/ViewController.m
+++ pageDemo/pageDemo/ViewController.m
@@ -30,22 +30,8 @@
        viewControllerAfterViewController:(UIViewController *)viewController{
     if ([viewController.view.backgroundColor isEqual:[UIColor blackColor]]) {
         return nil;
-    } 
-//    + (UIColor *)blackColor;      // 0.0 white
-//	+ (UIColor *)darkGrayColor;   // 0.333 white
-//	+ (UIColor *)lightGrayColor;  // 0.667 white
-//	+ (UIColor *)whiteColor;      // 1.0 white
-//	+ (UIColor *)grayColor;       // 0.5 white
-//	+ (UIColor *)redColor;        // 1.0, 0.0, 0.0 RGB
-//	+ (UIColor *)greenColor;      // 0.0, 1.0, 0.0 RGB
-//	+ (UIColor *)blueColor;       // 0.0, 0.0, 1.0 RGB
-//	+ (UIColor *)cyanColor;       // 0.0, 1.0, 1.0 RGB
-//	+ (UIColor *)yellowColor;     // 1.0, 1.0, 0.0 RGB
-//	+ (UIColor *)magentaColor;    // 1.0, 0.0, 1.0 RGB
-//	+ (UIColor *)orangeColor;     // 1.0, 0.5, 0.0 RGB
-//	+ (UIColor *)purpleColor;     // 0.5, 0.0, 0.5 RGB
-//	+ (UIColor *)brownColor;      // 0.6, 0.4, 0.2 RGB
-//	+ (UIColor *)clearColor;      // 0.0 white, 0.0 alpha
+    }
+	
 	UIColor* bgColor[] =
 	{
 		[UIColor blueColor],
@@ -71,6 +57,11 @@
 	{
 		index = 0;
 	}
+	
+	UILabel* lable = [[UILabel alloc] initWithFrame:[viewCtrl.view frame]];
+	viewCtrl.view = lable;
+	lable.text = [NSString stringWithFormat:@"color=%d, %@", index, [UIColor purpleColor]];
+	[lable release];
 	
 	viewCtrl.view.backgroundColor = bgColor[index];
 //    if ([viewController.view.backgroundColor isEqual:[UIColor blueColor]]) {

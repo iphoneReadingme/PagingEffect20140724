ae7ad69f1461ba0d97c4e797d1bdec83fb0f8b30
iphoneReadingme <readingme@126.com>
2014-07-24 10:58:41 +0800

    PagingEffect20140724　翻页动画demo

---
+++ PagingEffect20140724/PageTest2/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PageTest2
+//
+//  Created by apple on 13-9-13.
+//  Copyright (c) 2013年 kongyu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "PageAppViewController.h"
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end

d4bdf8bf776bc43c7331e799f8f6793439ed91d8
iphoneReadingme <readingme@126.com>
2014-07-25 09:30:52 +0800

    翻页方式和动画效果

--- PagingEffect20140724/pageDemo20140724/pageDemo/AppDelegate.m
+++ PagingEffect20140724/pageDemo20140724/pageDemo/AppDelegate.m
@@ -22,12 +22,20 @@
     [super dealloc];
 }
 
+
+
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     self.window = [[[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]] autorelease];
     // Override point for customization after application launch.
-    self.viewController = [[[NBPageViewController alloc] initWithTransitionStyle:UIPageViewControllerTransitionStylePageCurl 
-                                                          navigationOrientation:UIPageViewControllerNavigationOrientationHorizontal
+	
+	// style: 0.卷曲; 1.平滑
+	// orient: 0.左右卷曲/滑动; 1.上下卷曲/滑动
+	UIPageViewControllerTransitionStyle style[2] = {UIPageViewControllerTransitionStylePageCurl, UIPageViewControllerTransitionStyleScroll};
+	UIPageViewControllerNavigationOrientation orient[2] = {UIPageViewControllerNavigationOrientationHorizontal, UIPageViewControllerNavigationOrientationVertical};
+	
+    self.viewController = [[[NBPageViewController alloc] initWithTransitionStyle:style[1]
+                                                          navigationOrientation:orient[0]
                                                                         options:nil] autorelease];
     self.window.rootViewController = self.viewController;
     [self.window makeKeyAndVisible];

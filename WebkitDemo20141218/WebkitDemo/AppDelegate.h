2e2d6e9513ce32b234fe61f31864ddf19a34f741
iphoneReadingme <iphoneReadingme@users.noreply.github.com>
2014-12-17 17:02:12 +0800

    添加新工程WebkitDemo20141218

---
+++ WebkitDemo20141218/WebkitDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WebkitDemo
+//
+//  Created by yangfs on 14/12/17.
+//  Copyright (c) 2014年 yangfs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WebkitDemo20141218/WebkitDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WebkitDemo
+//
+//  Created by yangfs on 14/12/17.
+//  Copyright (c) 2014年 yangfs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+

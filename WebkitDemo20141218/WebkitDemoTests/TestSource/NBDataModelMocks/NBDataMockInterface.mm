1747cb52769c02e67c399c0133ca765a94dcb6f3
iphoneReadingme <iphoneReadingme@users.noreply.github.com>
2014-12-17 20:58:40 +0800

    添加Mock测试对象

---
+++ WebkitDemo20141218/WebkitDemoTests/TestSource/NBDataModelMocks/NBDataMockInterface.mm
@@ -0,0 +1,27 @@
+
+
+
+#import "NBDataMockInterface.h"
+
+
+
+@interface NBDataMockInterface()
+
+@end
+
+
+@implementation NBDataMockInterface
+
+
+- (void)dealloc
+{
+    [super dealloc];
+}
+
+
+- (NSString*)getChapterContent
+{
+	return nil;
+}
+
+@end
---
+++ WebkitDemo20141218/WebkitDemoTests/TestSource/NBDataModelMocks/NBMockChapterContent.h
@@ -0,0 +1,26 @@
+
+/*
+ *****************************************************************************
+ * Copyright (C) 2005-2014 UC Mobile Limited. All Rights Reserved
+ * File			: NBMockChapterContent.h
+ *
+ * Description	: 单元测试 Mock 接口：NBMockChapterContent
+ *
+ * Author		: yangfs@ucweb.com
+ *
+ * Created by yangfs on   2014-12-16.
+ * History		: modify: 2014-12-16.
+ *
+ ******************************************************************************
+ **/
+
+
+#import "NBDataMockInterface.h"
+
+
+@interface NBMockChapterContent : NBDataMockInterface
+
+- (NSString*)getChapterContent;
+
+@end
+

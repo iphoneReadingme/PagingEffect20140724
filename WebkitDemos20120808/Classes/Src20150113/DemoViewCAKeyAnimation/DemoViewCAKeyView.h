cf5b2c4ddecda233410a8cfa6a34d4075e2d2562
iphoneReadingme <iphoneReadingme@users.noreply.github.com>
2015-01-13 14:23:24 +0800

    关键帧动画测试demo

---
+++ WebkitDemos20120808/Classes/Src20150113/DemoViewCAKeyAnimation/DemoViewCAKeyView.h
@@ -0,0 +1,24 @@
+
+/*
+ *****************************************************************************
+ * Copyright (C) 2005-2014 UC Mobile Limited. All Rights Reserved
+ * File			: DemoViewCAKeyView.h
+ *
+ * Description	: 关键帧动画测试demo 视图
+ *
+ * Author		: yangfs@ucweb.com
+ *
+ * Created by yangfs on   2015-01-13.
+ * History		: modify: 2015-01-13.
+ *
+ ******************************************************************************
+ **/
+
+
+@interface DemoViewCAKeyView : UIScrollView
+{
+}
+
+
+@end
+
